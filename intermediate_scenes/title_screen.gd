ab4ca4dcbe17714d52cc7e4da0ded5a8d7eda993
buzzbuzz20xx <samirskyland@gmail.com>
2025-09-01 07:56:12 +0300

    Made the title screen awesome

--- intermediate_scenes/title_screen.gd
+++ intermediate_scenes/title_screen.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 @export var enemies: Array[PackedScene]
 
 func _ready() -> void:
+	%Title.text = Text.shake(%Title.text)
 	for scene: PackedScene in enemies:
 		var instance: Node = scene.instantiate()
 		assert(instance is Enemy, "Only put Enemy scenes in the enemies array")
@@ -11,10 +12,27 @@ func _ready() -> void:
 		button.add_theme_font_size_override("font_size", 50)
 		button.text = enemy.enemy_name
 		%BattlesContainer.add_child(button)
+		button.pivot_offset = button.size / 2
+		button.focus_entered.connect(
+			_on_focus_entered.bind(button)
+		)
+		button.focus_exited.connect(
+			_on_focus_exited.bind(button)
+		)
 		button.pressed.connect(
 			go_to_battle.bind(enemy)
 			)
 	%BattlesContainer.get_child(0).grab_focus()
+
+func _on_focus_entered(button: Button) -> void:
+	button.modulate.a = 1
+	%MoveSound.play()
+	var tween := get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_BOUNCE)
+	tween.tween_property(button, "scale", Vector2(1.5,1.5), 0.2)
+	tween.tween_property(button, "scale", Vector2(1,1), 0.1)
+
+func _on_focus_exited(button: Button) -> void:
+	button.modulate.a = 0.5
 
 func go_to_battle(enemy: Enemy) -> void:
 	Battle.enemy = enemy
